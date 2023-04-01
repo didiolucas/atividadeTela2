12537e745277765ed0768370f3f20ec61007365f
DidioDev <lucassmaniottoo@gmail.com>
2023-04-01 15:26:43 -0300

    Upando arquivos

---
+++ styles.js
@@ -0,0 +1,94 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+
+  scrollView: {
+    backgroundColor: '#FFF',
+    paddingHorizontal:15,
+    paddingTop:40
+  },
+  container:{
+    alignItems: 'center',
+  },
+  logo:{
+    width:150,
+    height:150
+  },
+  h1:{
+    color:'#000',
+    fontSize:27,
+    fontWeight:'bold',
+    marginVertical:10
+
+  },
+  h2:{
+    color:'#999',
+    fontSize:15
+  },
+  inputArea:{
+    width:'100%',
+    paddingTop:20,
+    fontWeight:'bold',
+
+
+  },
+  inputLabel:{
+    color:'#777',
+    fontSize:14,
+    marginBottom:7
+
+  },
+  inputField:{
+    borderWidth:2,
+    borderRadius:5,
+    borderColor:'#DDD',
+    fontSize:13,
+    padding:10,
+    color:'#a6a6a6'
+
+  },
+  aditionals:{
+
+  },
+  forgotBtnArea:{
+
+  },
+  forgotBtnText:{
+    color:'#395bb3',
+    fontWeight:'bold',
+    marginLeft:'170px',
+    padding:'12px',
+    alignSelf:'right'
+
+  },
+  button:{
+    backgroundColor:'#4162b7',
+    width:"300px",
+    textAlign:'center',
+    padding:'11px',
+    borderRadius:'5px'
+
+  },
+  buttonText:{
+    color:'white',
+    fontSize:'15px'
+  },
+  signUpArea:{
+
+  },
+  signUpText:{
+    color:'#a6a6a6',
+    paddingTop:'35px'
+  },
+  signUpBtnText:{
+    color:'#395bb3',
+    fontWeight:'bold',
+  },
+  footerArea:{
+    paddingTop:'50px'
+  },
+  footerText:{
+    color:'#a6a6a6'
+  }
+
+});
